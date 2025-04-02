de1b5860b6564c969664b5fb05d3e0cd6fbdf455
javad <javad_design@yahoo.com>
2025-04-02 16:58:20 +0430

    Change: add divider and update about me in homepage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="container mx-auto relative p-16 scroll-my-12">
-    <section class="w-full space-y-8 max-w-2xl mx-auto">
+    <section class="w-full space-y-8 max-w-5xl mx-auto">
 
       <TopMenu />
 
@@ -10,11 +10,13 @@
 
       <section class="flex min-h-0 flex-col gap-y-3" v-if="!$route.meta.hideLayout">
         <h2 class="text-xl font-bold">About</h2>
-        <div class="font-mono text-sm">
-          Frontend-focused Full Stack Engineer specializing in high-performance React applications, scalable Node.js
-          services, and real-time collaboration systems. Experienced in technical architecture design and remote team
-          leadership.
+        <div class="font-mono text-sm text-justify">
+          I am a Hydroinformatics Specialist with a strong background in web development, Python programming, and
+          data-driven decision-making for water resource management.
+          I am passionate about bridging the gap between hydrology, data science, and web technologies to develop
+          next-generation hydroinformatics solutions.
         </div>
+        <Divider />
       </section>
 
       <router-view></router-view>
@@ -25,6 +27,8 @@
 <script setup>
 import TopMenu from './components/TopMenu.vue'
 import AboutMe from './components/AboutMe.vue'
+import Divider from 'primevue/divider';
+
 </script>
 
 <style scoped></style>

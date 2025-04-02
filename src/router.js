29781dddb547f7132e036926d0ce6c66c6ddb540
javad <javad_design@yahoo.com>
2025-04-02 17:55:16 +0430

    Change: add gh-pages and predeploy & deploy to package.json Change: erplace createWebHistory with createWebHashHistory to fix page loading on github pages

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory , createWebHistory } from 'vue-router';
 import allRoutes from './allRoutes';
 // Import other route modules as needed
 
@@ -8,7 +8,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
